53954f0c1d036d2c1baeeda5c83be80cc9bd16e5
Thiago Valentim <thiago.valentim@tripda.com.br>
2015-05-08 14:59:02 +0000

    I added setter to http_client

--- lib/consumers/consumer_base.rb
+++ lib/consumers/consumer_base.rb
@@ -3,8 +3,20 @@ require 'sneakers'
 class ConsumerBase
   include Sneakers::Worker
 
-  def initialize(http_client)
-    @http_client = http_client
+  attr_accessor :http_client
+
+  def initialize()
+    super()
+    @config = YAML.load_file(File.dirname(__FILE__)+"/../../config/config.yml")
+    @http_client = ::Faraday::Connection.new url: @config['elastisearch.host']
+  end
+
+  def run_request(method, path, body)
+    @http_client.run_request \
+      method.downcase.to_sym,
+      path,
+      ( body ? MultiJson.dump(body): nil ),
+      {'Content-Type' => 'application/json'}
   end
 
   def work(msg)
